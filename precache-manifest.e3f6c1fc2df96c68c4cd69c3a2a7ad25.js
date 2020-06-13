1ca7b0b3bb75165f8cc6acffe721f27309ca685f
llsanketll <sanket.lamsal@gmail.com>
2020-06-13 13:12:53 +0545

    Updates

---
+++ precache-manifest.e3f6c1fc2df96c68c4cd69c3a2a7ad25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8693f3d73bd59c8bc9d88ae7e0707f1",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "a30d209b5cf03ad7bac3",
+    "url": "/Todo-List/static/css/main.0283d31d.chunk.css"
+  },
+  {
+    "revision": "d4e69eb2e1556f82b83f",
+    "url": "/Todo-List/static/js/2.1de5dd00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List/static/js/2.1de5dd00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30d209b5cf03ad7bac3",
+    "url": "/Todo-List/static/js/main.e4efae4f.chunk.js"
+  },
+  {
+    "revision": "22c3c29c1b767a1449c0",
+    "url": "/Todo-List/static/js/runtime-main.8b916450.js"
+  }
+]);
